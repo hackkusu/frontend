2d317ac499e88864bc66aaa4827818466b3bae8a
chase2981 <chasewgibbons@gmail.com>
2024-04-21 22:24:16 -0600

    added achievements

--- src/components/CommonForBoth/TopbarDropdown/NotificationDropdown.js
+++ src/components/CommonForBoth/TopbarDropdown/NotificationDropdown.js
@@ -48,6 +48,12 @@ const NotificationDropdown = props => {
       newNotifications.push(data);
       setNotifications(newNotifications);
     });
+    channel.bind('new-achievement', (data) => {
+      setNotificationsCount(notificationsCount + 1);
+      const newNotifications = notifications;
+      newNotifications.push(data);
+      setNotifications(newNotifications);
+    });
 
     // Cleanup function
     return () => {
@@ -141,6 +147,7 @@ const NotificationDropdown = props => {
             </Link> */}
 
             {notifications.map((item, idx) => (
+              item.message == 'A new response was added.' ? (
               <Link key={idx} to="" className="text-dark notification-item">
                 <div className="d-flex align-items-start">
                   <div className="avatar-xs me-3">
@@ -165,7 +172,30 @@ const NotificationDropdown = props => {
                     </div>
                   </div>
                 </div>
-              </Link>
+              </Link>) : (
+                <Link key={idx} to="" className="text-dark notification-item">
+                <div className="d-flex align-items-start">
+                  <div className="avatar-xs me-3">
+                    <span className="avatar-title bg-primary rounded-circle font-size-16">
+                    <i className="uil-shopping-basket"></i>
+                    </span>
+                  </div>
+                  <div className="flex-1">
+                    <h6 className="mt-0 mb-1">
+                    {item.message} 🏆🏆
+                    </h6>
+                    <div className="font-size-12 text-muted">
+                      <p className="mb-1">
+                      on {item.survey_name} survey using "{item.survey_start_code}" start code 
+                      </p>
+                      <p className="mb-0">
+                      <i className="mdi mdi-clock-outline"></i>{" "}
+                        just now
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </Link>)
             ))}
 
             {/* <Link to="" className="text-dark notification-item">
